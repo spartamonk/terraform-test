fbbd0413bad76ac2baba985a2a6020ebaf9891ba
spartamonk <cm.akoto@yahoo.com>
2024-12-06 21:19:17 -0600

    added ec2

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-# resource "aws_instance" "web2" {
-#   ami           = "ami-0453ec754f44f9a4a"
-#   instance_type = "t2.micro"
+resource "aws_instance" "web2" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
 
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 terraform {
   required_providers {
     aws = {
